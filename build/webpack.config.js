d88f61c44de8b4c417b9a2a95910983bcf2990c1
lynsun <814253326@qq.com>
2017-01-05 16:33:32 +0800

    chore: use devServer

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,18 +1,26 @@
 var path = require('path')
 
+var outputPath = path.resolve(__dirname ,'dist');
+console.log('outputPath:',outputPath);
+var entryPath = path.resolve(__dirname, '../index.js');
+
 module.exports = {
   entry: {
-    app: './index.js',
+    app: entryPath,
   },
   output: {
-    path: './dist',
+    path: outputPath,
+    publicPath: '/build/',
     filename: '[name].js'
   },
   module: {
     rules: [
       {
-        test: /\.(js)$/, use: 'babel-loader'
+        test: /\.(js)$/, use: 'babel-loader',exclude: /node_modules/
       }
     ]
+  },
+  performance: {
+    hints: process.env.NODE_ENV === 'production' ? "warning" : false
   }
 }
